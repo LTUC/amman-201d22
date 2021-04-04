05002bcb38efd900d4055d55714e2adfcaca352f
bashar-alasmar <basharalasmar96@gmail.com>
2021-04-04 20:59:49 +0300

    demo

---
+++ class-01/demo/js/app.js
@@ -0,0 +1,60 @@
+'use strict';
+
+//Run our javascript code in a strict mode!
+
+
+// Strings "whatever" 'whatever'
+
+// numbers 122321  3.5 -3 
+
+//Booleans True Or False
+
+
+// Variables 
+
+// let, const
+// let userName = 'Bashar';
+
+
+// if(5 > 2){
+//     var x = 5;
+// }
+// console.log(x);
+
+// let x = 10;
+
+// x = 20; re assigning!
+// console.log(x);
+
+// const url = 'https://www.google.com';
+
+// let user = 'Bashar';
+// let x = 10;
+// let anything = 'Hello World';
+// for(let i = 0 ; i <10 ; i++){
+//     console.log(anything ,i);
+// }
+// console.log(typeof 555);
+
+// let userName = prompt('what\'s your name?');
+// // whatever we retrieve from the prompt is going to be a String
+// console.log(typeof userName);
+// let userAge = prompt("what's your age?");
+// console.log(userAge);
+
+// let favArtist = prompt("whats your favourite artist?");
+// console.log(favArtist);
+
+// let typeOfArt = prompt('what art do you like?');
+// console.log(typeOfArt);
+
+// // concatination 
+// alert('Your name is ' + userName + ' and your age is ' + userAge + ' your favourite artist is ' + favArtist + ' the type of art you like is ' + typeOfArt);
+
+
+let num1 = prompt('insert the first number');
+'26'
+num1 = parseInt(num1) // 26 
+let num2 = prompt('insert the second number');
+num2= parseInt(num2)
+alert(num1 + num2 ); //1020
