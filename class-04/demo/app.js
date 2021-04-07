27f6849c408f61662149660e5c870175906f9b17
bashar-alasmar <basharalasmar96@gmail.com>
2021-04-07 15:34:47 +0300

    demo

---
+++ class-04/demo/app.js
@@ -0,0 +1,104 @@
+'use strict';
+// ctrl + / 
+// function welcomingMessage(user){
+//     alert('Welcome to the website' );
+//     console.log('Inside the function')
+// }
+
+// welcomingMessage();
+// welcomingMessage();
+// welcomingMessage();
+// welcomingMessage();
+// welcomingMessage();
+// welcomingMessage();
+// welcomingMessage();
+// let userFirst = prompt('Enter your name please');
+// let userLast = prompt('Enter your last name ');
+// // function getName(firstName, LastName){ // Parameter
+// //     // console.log(LastName);
+// //     let output = 'Hello ' + firstName + LastName;
+// //     return output;
+// // }
+
+// console.log(getName(userFirst, userLast)); // argument
+// console.log(getName('Omar'));
+// console.log(getName('Jana'));
+
+// console.log(sumAndMultiply(5,9));
+// function sumAndMultiply(num1,num2){
+//     let sum = num1+ num2;
+//     let multiply = num1 * num2;
+//     return [sum,multiply,num1,num2]; // the end of the function task!
+
+// }
+
+// console.log(sumAndMultiply(8,9));
+// console.log(sumAndMultiply(7,7)[0]);
+// console.log(sumAndMultiply(5,4)[1]);
+
+
+// function welcomingMessage(){
+//     let a = 7
+//     return a;
+//     alert('HEllo User');
+// }
+
+// console.log(welcomingMessage());
+// function getSquare (length){
+//     let calc = length * length;
+// }
+// console.log(getSquare(8));
+
+// function expression 
+// let userAge = prompt('Whats your date of birth?');
+// userAge = parseInt(userAge)
+// console.log(anything);
+// let anything = 10 ;
+
+// let getAge = function(year){
+//     let calc = 2021- year;
+//     return calc;
+// }
+// console.log(getAge(1993));
+
+// let valueOfFunction = getAge(userAge); // <= the value of the function
+//  console.log(valueOfFunction);
+
+// // console.log(getAge(1996));
+// // console.log(getAge(1800));
+// // console.log(getAge(999));
+
+//Scopes 
+// let lastName = 'Alasmar';
+// if(true){
+//     let firstName = 'Bashar';
+//     console.log(lastName);
+// }
+// console.log(firstName);
+// console.log(firstName);
+// let num3 = 10
+// // let user = 'Bashar';
+// function calc(num1,num2){
+//     let user = 'ahmad?';
+//     // console.log(user);
+//     num3 = 20; // 20
+//     let division = num1/num2;
+//     return user;
+// }
+// console.log(num3);
+
+// console.log(calc(20,30)); // 
+
+// console.log(num3);
+
+// console.log(num3);
+
+// function anything(){
+    
+// }
+
+// let k = 20
+// if(true){
+//     k = 40;
+// }
+// console.log(k);
